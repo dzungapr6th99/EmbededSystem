5d0d9fb2ca7b73fb8c1381a303cd5c0d89bfde5e
dzung.apr6th@gmail.com <dzung.apr6th@gmail.com>
2022-07-15 11:04:46 +0700

    Add project files.

---
+++ Client/TCPClient/TcpClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using 
+namespace Client.TCPClient
+{
+    class TcpClient
+    {
+    }
+}
---
+++ GateWay/TCPServer/PeerConnected.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace GateWay.TCPServer
+{
+    class PeerConnected
+    {
+        private Socket AcceptedSocket;
+        public Queue<String> ReceiveMessages = new Queue<string>();
+        public Queue<String> SendMessage = new Queue<string>();
+        NetworkStream _NetworkStream;
+        StreamReader Reader;
+        StreamWriter Writer;
+        Thread ReadData;
+        Thread Response;
+        public PeerConnected(Socket _socket)
+        {
+            AcceptedSocket = _socket;
+            _NetworkStream = new NetworkStream(AcceptedSocket);
+            Reader = new StreamReader(_NetworkStream);
+            Writer = new StreamWriter(_NetworkStream);
+            
+        }
+        ~PeerConnected()
+        {
+            ReadData.Abort();
+            Response.Abort();
+            ReceiveMessages.Clear();
+            SendMessage.Clear();
+
+        }
+        public void Start()
+        {
+            ReadData = new Thread(() => 
+            {
+                while (true)
+                {
+                    String ReadData = Reader.ReadLine();
+                    if (!String.IsNullOrEmpty(ReadData))
+                        ReceiveMessages.Enqueue(ReadData);
+                }
+            });
+            Response = new Thread(()=>
+            {
+                while (true)
+                {
+                    if (SendMessage.Count != 0)
+                    {
+                        String Message = SendMessage.Dequeue();
+                        Writer.WriteLine(Message);
+
+                    }
+                    else
+                        Thread.Sleep(1000);
+                }    
+            });
+            ReadData.Start();
+            Response.Start();
+
+        }
+       
+    }
+}
